b28ecd7fa044b83a19c9742033e5f7b107c9e966
Andres Anni <andres.anni1985@gmail.com>
2023-04-29 16:33:53 -0300

    Codigo formateado. Logica funcionando. Empiezan los ajustes

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const doorElements = document.querySelectorAll('.door');
-const tableroElement = document.getElementById('tablero');
+const doorElements = document.querySelectorAll(".door");
+const tableroElement = document.getElementById("tablero");
 
 //Elementos html
-let  selectedDoorElement;
+let selectedDoorElement;
 let openedDoorElement;
 let prizeDoorElement;
 
@@ -11,152 +11,139 @@ let selectedDoorObject;
 let openedDoorObject;
 let prizeDoorObject;
 
-
-const doors=[];
+const doors = [];
 
 assignDoorContent();
 assignDoorListeners();
 
+function assignDoorContent() {
+  const randomNumber = Math.floor(Math.random() * 3);
 
-function assignDoorContent(){
-    
-    const randomNumber=Math.floor((Math.random()*3));
-    
+  for (let i = 0; i <= 2; i++) {
+    let hasPrize = false;
 
-    for(let i=0;i<=2;i++){        
-        let hasPrize = false;
-
-        if(i===randomNumber){
-            hasPrize=true;
-            prizeDoorElement = document.querySelector(`[data-door="${i}"]`);
-        }                
-        const door = {number : i, 
-                     opened:false,
-                     selected:false, 
-                     prize:hasPrize, 
-                     avaiable:true} 
-
-        doors.push(door);        
-    }       
-    
-    prizeDoorObject = doors[randomNumber];
-
-    console.group("----ASIGNACION INICIAL DE PUERTAS----");
-    console.log("NUMERO ALEATORIO:");
-    console.log(randomNumber);
-    console.log("ELEMENTO GANADOR:");
-    console.log(prizeDoorElement);
-    console.log("OBJETO GANADOR:");
-    console.log(prizeDoorObject);
-    console.groupEnd();
-}
-
-function assignDoorListeners(){
-    
-    doorElements.forEach((doorElement=>{
-            doorElement.addEventListener('click',()=>{
-
-                const doorIndex = parseInt(doorElement.dataset.door); 
-                selectedDoorObject = doors[doorIndex]; 
-
-                selectedDoorObject.selected=true;
-                selectedDoorObject.avaiable=false;
-
-                doorElement.classList.add('selected');               
-                selectedDoorElement=doorElement;
-
-                const p = document.createElement("p");
-                p.innerText="Puerta seleccionada : " + (selectedDoorObject.number +1);
-                tableroElement.appendChild(p);
-
-                console.group("----SELECCION DE PUERTA INICIAL----");
-                console.log("PUERTA SELECCIONADA EN ELEMENTO HTML:");
-                console.log(selectedDoorElement);
-                console.log("PUERTA SELECCIONADA EN OBJETO JS:");
-                console.log(selectedDoorObject);
-                console.groupEnd();
-
-
-
-                secondInstance(selectedDoorObject);
-                
-            }
-            )
-    }));
-    
-    
-}
-
-
-function secondInstance(selectedDoor){//Le damos la puerta que eligio el participante y nos devuelve la puerta que le podemos abrir
-    
-        const openableDoors = doors.filter(door=>door.number!==selectedDoor.number);
-        let openableDoorIndex;
-
-        const noPrizeDoors = openableDoors.every(door => !door.prize); //Evaluo el caso en el que no hayan premios
-        
-        if (noPrizeDoors) {
-            openableDoorIndex = Math.floor((Math.random() * 2)); //Si no hay premio, ambas son seleccionables , por lo que selecciono una al azar
-        } else {
-            openableDoorIndex = openableDoors.findIndex(door => !door.prize); //Si hay premio, selecciono la puerta que no tiene premio
-        }
-        
-        openedDoorElement = document.querySelector(`[data-door="${openableDoors[openableDoorIndex].number}"]`);
-        openedDoorObject = openableDoors[openableDoorIndex];
-        
-        
-        let p = document.createElement("p");
-        p.innerText= "El Presentador a continuacion abre la puerta numero : " + (openableDoors[openableDoorIndex].number + 1) ;
-        tableroElement.appendChild(p);
-
-        p = document.createElement("p");
-        p.innerText= "Quieres cambiar la puerta o mantienes la elegida?";
-
-        const changeButton = document.createElement("button");
-        changeButton.innerText="Cambiar";
-        const keepButton = document.createElement("button");
-        keepButton.innerText="Mantener";
-
-        tableroElement.appendChild(p);
-        
-        tableroElement.appendChild(changeButton);
-        changeButton.addEventListener('click',()=>thirdInstance(true));
-
-        tableroElement.appendChild(keepButton);
-        keepButton.addEventListener('click',()=>thirdInstance(false));        
-}
-
-
-function thirdInstance(withChange){
-
-    if(withChange){
-
-        const targetDoorToChange = doors.find(door =>door.avaiable);
-        selectedDoorElement =  document.querySelector(`[data-door="${targetDoorToChange.number}"]`);
-        selectedDoorObject = targetDoorToChange;
+    if (i === randomNumber) {
+      hasPrize = true;
+      prizeDoorElement = document.querySelector(`[data-door="${i}"]`);
     }
-    
-    if(selectedDoorObject.prize){
+    const door = {
+      number: i,
+      opened: false,
+      selected: false,
+      prize: hasPrize,
+      avaiable: true,
+    };
 
-            let p = document.createElement("p");
-            p.innerText="Has Ganado!"
-            tableroElement.appendChild(p);
-        }
-        else{
-            let p = document.createElement("p");
-            p.innerText="Has Perdido!"
-            tableroElement.appendChild(p);
-        
-        }
+    doors.push(door);
+  }
 
-    }
-        
+  prizeDoorObject = doors[randomNumber];
 
-    
+  console.group("----ASIGNACION INICIAL DE PUERTAS----");
+  console.log("NUMERO ALEATORIO:");
+  console.log(randomNumber);
+  console.log("ELEMENTO GANADOR:");
+  console.log(prizeDoorElement);
+  console.log("OBJETO GANADOR:");
+  console.log(prizeDoorObject);
+  console.groupEnd();
+}
 
+function assignDoorListeners() {
+  doorElements.forEach((doorElement) => {
+    doorElement.addEventListener("click", () => {
+      const doorIndex = parseInt(doorElement.dataset.door);
+      selectedDoorObject = doors[doorIndex];
 
+      selectedDoorObject.selected = true;
+      selectedDoorObject.avaiable = false;
 
+      doorElement.classList.add("selected");
+      selectedDoorElement = doorElement;
 
+      const p = document.createElement("p");
+      p.innerText = "Puerta seleccionada : " + (selectedDoorObject.number + 1);
+      tableroElement.appendChild(p);
 
+      console.group("----SELECCION DE PUERTA INICIAL----");
+      console.log("PUERTA SELECCIONADA EN ELEMENTO HTML:");
+      console.log(selectedDoorElement);
+      console.log("PUERTA SELECCIONADA EN OBJETO JS:");
+      console.log(selectedDoorObject);
+      console.groupEnd();
 
+      secondInstance(selectedDoorObject);
+    });
+  });
+}
 
+function secondInstance(selectedDoor) {
+  const openableDoors = doors.filter(
+    (door) => door.number !== selectedDoor.number
+  );
+  let openableDoorIndex;
+
+  const noPrizeDoors = openableDoors.every((door) => !door.prize);
+
+  if (noPrizeDoors) {
+    openableDoorIndex = Math.floor(Math.random() * 2);
+  } else {
+    openableDoorIndex = openableDoors.findIndex((door) => !door.prize);
+  }
+
+  openedDoorElement = document.querySelector(
+    `[data-door="${openableDoors[openableDoorIndex].number}"]`
+  );
+  openedDoorObject = openableDoors[openableDoorIndex];
+
+  let p = document.createElement("p");
+  p.innerText =
+    "El Presentador a continuacion abre la puerta numero : " +
+    (openableDoors[openableDoorIndex].number + 1);
+  tableroElement.appendChild(p);
+
+  p = document.createElement("p");
+  p.innerText = "Quieres cambiar la puerta o mantienes la elegida?";
+
+  const changeButton = document.createElement("button");
+  changeButton.innerText = "Cambiar";
+  const keepButton = document.createElement("button");
+  keepButton.innerText = "Mantener";
+
+  tableroElement.appendChild(p);
+
+  tableroElement.appendChild(changeButton);
+  changeButton.addEventListener("click", () => thirdInstance(true));
+
+  tableroElement.appendChild(keepButton);
+  keepButton.addEventListener("click", () => thirdInstance(false));
+
+  console.group("----SELECCION DE PUERTA A ABRIR----");
+  console.log("PUERTAS RESTANTES PARA ABRIR:");
+  console.log(openableDoors);
+  console.log("PUERTA SELECCIONADA PARA ABRIR EN ELEMENTO HTML:");
+  console.log(openedDoorElement);
+  console.log("PUERTA SELECCIONADA PARA ABRIR EN OBJETO JS:");
+  console.log(openedDoorObject);
+  console.groupEnd();
+}
+
+function thirdInstance(withChange) {
+  if (withChange) {
+    const targetDoorToChange = doors.find((door) => door.avaiable);
+    selectedDoorElement = document.querySelector(
+      `[data-door="${targetDoorToChange.number}"]`
+    );
+    selectedDoorObject = targetDoorToChange;
+  }
+
+  if (selectedDoorObject.prize) {
+    let p = document.createElement("p");
+    p.innerText = "Has Ganado!";
+    tableroElement.appendChild(p);
+  } else {
+    let p = document.createElement("p");
+    p.innerText = "Has Perdido!";
+    tableroElement.appendChild(p);
+  }
+}
